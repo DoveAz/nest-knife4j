8f95769dcc404ec93bce800e775c71e48647b101
DoveAz <lijianmin1993@gmail.com>
2021-12-11 16:22:18 +0800

    ⬆️knife4j 升级到2.0.9

--- examples/src/app.controller.ts
+++ examples/src/app.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiExtension, ApiOperation, ApiTags } from '@nestjs/swagger';
 @ApiTags('小猫咪')
-@Controller()
+@Controller('cat')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiExtension('x-author', 'DoveAz')
   @ApiOperation({
     summary: '哈哈',
+    description: '你好啊',
   })
   @Get()
   getHello(): string {
--- examples/src/main.ts
+++ examples/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
       },
     ],
   });
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
